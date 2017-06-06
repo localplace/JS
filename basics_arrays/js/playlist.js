53a7a50cf5ba0db0df320d14552bdd045738ef82
Malathi <dharmalingam.malathi@gmail.com>
2017-06-06 06:27:59 +0530

    Update New Array Theory

--- basics_arrays/js/playlist.js
+++ basics_arrays/js/playlist.js
@@ -2,3 +2,7 @@ var playlist = [];
 playlist.push("I did it my way");
 playlist.unshift("Respect");
 printList(playlist);
+var lastItem = playlist.pop();
+var first = playlist.shift();
+document.write(lastItem);
+document.write(first);
